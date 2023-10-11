59bb3457b46b0cfc68e543930eace33cb08f901e
trentiX <130845163+trentiX@users.noreply.github.com>
2023-10-11 17:45:58 +0600

    Creating a new project and first commits

---
+++ Bomberman/Assets/Scripts/MovementController.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public new Rigidbody2D rigidbody { get; private set; }
+    private Vector2 direction = Vector2.down;
+    public float speed = 5f;
+
+    public KeyCode inputUp = KeyCode.W;
+    public KeyCode inputDown = KeyCode.S;
+    public KeyCode inputLeft = KeyCode.A;
+    public KeyCode inputRight = KeyCode.D;
+
+    public AnimatedSpriteRenderer spriteRendererUp;
+    public AnimatedSpriteRenderer spriteRendererDown;
+    public AnimatedSpriteRenderer spriteRendererLeft;
+    public AnimatedSpriteRenderer spriteRendererRight;
+    private AnimatedSpriteRenderer activeSpriteRenderer;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        activeSpriteRenderer = spriteRendererDown;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(inputUp))
+        {
+            SetDiretion(Vector2.up, spriteRendererUp);
+        }
+        else if (Input.GetKey(inputDown))
+        {
+            SetDiretion(Vector2.down, spriteRendererDown);
+        }
+        else if (Input.GetKey(inputLeft))
+        {
+            SetDiretion(Vector2.left, spriteRendererLeft);
+        }
+        else if (Input.GetKey(inputRight))
+        {
+            SetDiretion(Vector2.right, spriteRendererRight);
+        }
+        else
+        {
+            SetDiretion(Vector2.zero, activeSpriteRenderer);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rigidbody.position;
+        Vector2 translation = direction * speed * Time.fixedDeltaTime;
+
+        rigidbody.MovePosition(position + translation);
+    }
+
+    private void SetDiretion(Vector2 newDirection, AnimatedSpriteRenderer spriteRenderer)
+    {
+        direction = newDirection;
+
+        spriteRendererUp.enabled = spriteRenderer == spriteRendererUp;
+        spriteRendererDown.enabled = spriteRenderer == spriteRendererDown;
+        spriteRendererLeft.enabled = spriteRenderer == spriteRendererLeft;
+        spriteRendererRight.enabled = spriteRenderer == spriteRendererRight;
+
+        activeSpriteRenderer = spriteRenderer;
+        activeSpriteRenderer.idle = direction == Vector2.zero;
+    }
+}
