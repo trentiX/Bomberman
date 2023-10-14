6e26f68703cf78581efd66e54b454c810fd98fbd
trentiX <130845163+trentiX@users.noreply.github.com>
2023-10-14 17:18:56 +0600

    Final commit

--- Bomberman/Assets/Scripts/MovementController.cs
+++ Bomberman/Assets/Scripts/MovementController.cs
@@ -94,5 +94,6 @@ public class MovementController : MonoBehaviour
     private void OnDeathSequenceEnded()
     {
         gameObject.SetActive(false);
+        FindObjectOfType<GameManager>().CheckWinState();
     }
 }
